11ff699eaa0a7e86c5e1cc12da15040e8e3ce9a0
1043717432 <eric824@vip.qq.com>
2025-11-20 10:25:11 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -23,8 +23,9 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 #git clone -b js https://github.com/gngpp/luci-theme-design package/luci-theme-design
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
-#git clone https://github.com/sirpdboy/netspeedtest package/homebox
-#git clone https://github.com/destan19/OpenAppFilter package/openappfilter
+git clone https://github.com/sirpdboy/netspeedtest package/homebox
+git clone https://github.com/destan19/OpenAppFilter package/openappfilter
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 #rm -rf feeds/packages/net/open-app-filter
 #git clone https://github.com/linkease/nas-packages-luci package/nas-packages-luci
 #git clone https://github.com/linkease/nas-packages package/nas-packages
